580895ffc2604f9988f4315ed281ec544150726b
Ankit <ankitupa7799@gmail.com>
2021-07-11 10:08:18 +0530

    Updates

---
+++ precache-manifest.0511abae6a172f991e44ec497aae9dbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7217d8738d4c7b5db78878c7419595ff",
+    "url": "/Movieapitask/index.html"
+  },
+  {
+    "revision": "287038fd83ea942879fa",
+    "url": "/Movieapitask/static/css/2.b0704b7a.chunk.css"
+  },
+  {
+    "revision": "bce5f638e23d3e6d24dd",
+    "url": "/Movieapitask/static/css/main.91726ff9.chunk.css"
+  },
+  {
+    "revision": "287038fd83ea942879fa",
+    "url": "/Movieapitask/static/js/2.48bfdbef.chunk.js"
+  },
+  {
+    "revision": "774152ec3dfb5dadc6880f2e7f79e400",
+    "url": "/Movieapitask/static/js/2.48bfdbef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bce5f638e23d3e6d24dd",
+    "url": "/Movieapitask/static/js/main.11fc5740.chunk.js"
+  },
+  {
+    "revision": "0ddf6bd17a77018e8f7b",
+    "url": "/Movieapitask/static/js/runtime-main.3006ab72.js"
+  }
+]);
